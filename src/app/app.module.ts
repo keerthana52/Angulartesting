4cf816ed331431e67ea00b51aef5bf4d5c45b338
Keerthana <purumandlakeerthana@gmail.com>
2021-05-20 23:06:47 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserRegistrationComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
